6da661fc86ac92edcc29c170b12cee0067e4b21c
Honcharuk Oleksii <oleksiugoncaruk@gmail.com>
2025-09-13 13:33:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,42 +1,27 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Введiть порядковий номер знака зодiаку (1-12): ");
+        Console.Write("Введiть номер знака зодiаку (1-12): ");
+        int n = int.Parse(Console.ReadLine());
 
-        if (int.TryParse(Console.ReadLine(), out int number))
+        switch (n)
         {
-            string zodiac;
-
-            switch (number)
-            {
-                case 1: zodiac = "Овен"; break;
-                case 2: zodiac = "Тiлець"; break;
-                case 3: zodiac = "Близнюки"; break;
-                case 4: zodiac = "Рак"; break;
-                case 5: zodiac = "Лев"; break;
-                case 6: zodiac = "Дiва"; break;
-                case 7: zodiac = "Терези"; break;
-                case 8: zodiac = "Скорпiон"; break;
-                case 9: zodiac = "Стрiлець"; break;
-                case 10: zodiac = "Козорiг"; break;
-                case 11: zodiac = "Водолiй"; break;
-                case 12: zodiac = "Риби"; break;
-                default:
-                    Console.WriteLine("Помилка: номер має бути вiд 1 до 12.");
-                    return;
-            }
-
-            Console.WriteLine($"Знак зодiаку: {zodiac}");
+            case 1: Console.WriteLine("Овен"); break;
+            case 2: Console.WriteLine("Тiлець"); break;
+            case 3: Console.WriteLine("Близнюки"); break;
+            case 4: Console.WriteLine("Рак"); break;
+            case 5: Console.WriteLine("Лев"); break;
+            case 6: Console.WriteLine("Дiва"); break;
+            case 7: Console.WriteLine("Терези"); break;
+            case 8: Console.WriteLine("Скорпiон"); break;
+            case 9: Console.WriteLine("Стрiлець"); break;
+            case 10: Console.WriteLine("Козорiг"); break;
+            case 11: Console.WriteLine("Водолiй"); break;
+            case 12: Console.WriteLine("Риби"); break;
+            default: Console.WriteLine("Помилка: номер має бути вiд 1 до 12."); break;
         }
-        else
-        {
-            Console.WriteLine("Помилка: введено не число.");
-        }
-
-        Console.WriteLine("Натиснiть будь-яку клавішу для завершення...");
-        Console.ReadKey();
     }
 }
